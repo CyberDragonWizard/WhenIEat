3c271065a4913d7d3ec5db39f80a02a136d583eb
Brian <briand.nester@gmail.com>
2020-12-01 17:28:48 -0500

    push

--- client/when-i-eat/src/shared/Header.jsx
+++ client/when-i-eat/src/shared/Header.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import LogoImg from '../Assets/logo.png';
 import './Header.css'
 
 const Header = () => {
     return (
         <div className='header'> 
-            <img className='header-logo' src={LogoImg} alt='logo' height='130' width='130' />
+            
         </div>
     );
 };
--- client/when-i-eat/src/shared/Navbar.jsx
+++ client/when-i-eat/src/shared/Navbar.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import './Navbar.css'
+import { NavLink } from 'react-router-dom';
 
 export default function Navbar() {
     return (
         <div className='navbar'>
+            <div className='nav-button-div'>
+
+                <NavLink to='/home'>
+                    <button type='submit' className='nav-button'>Enter Calories</button>
+                </NavLink>
+
+                <NavLink to='/stats'>
+                    <button type='submit' className='nav-button'>View Stats</button>
+                </NavLink>
+
+            </div>   
         </div>
     )
 }
