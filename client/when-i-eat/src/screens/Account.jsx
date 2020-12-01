95af072de46b002b354bd70dfc25efab692cfe31
Brian <briand.nester@gmail.com>
2020-12-01 15:04:55 -0500

    push

--- client/when-i-eat/src/screens/Account.jsx
+++ client/when-i-eat/src/screens/Account.jsx
@@ -3,11 +3,16 @@ import './Account.css'
 import HomeFooter from '../shared/HomeFooter';
 import Header from '../shared/Header';
 import Navbar from '../shared/Navbar';
+import ProfilePlaceholder from '../Assets/profile-placeholder.png'
 
 function Account() {
     return (
         <div>
             <Header />
+            <button type='submit' className='delete-button'>Delete Account</button>
+            <div className='account-info'>
+                <img className='profile-placeholder' src={ProfilePlaceholder} alt='logo' height='130' width='130' />
+            </div>
             <Navbar />
             <div>
                 <HomeFooter /> 
