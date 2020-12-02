af624b94caec0c705520fbad01d9b7e32f4e88d4
Brian <briand.nester@gmail.com>
2020-12-02 17:43:18 -0500

    push

--- client/when-i-eat/src/shared/Navbar.jsx
+++ client/when-i-eat/src/shared/Navbar.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './Navbar.css'
 import { NavLink } from 'react-router-dom';
+import ProfilePlaceholder from '../Assets/profile-placeholder.png'
 
 export default function Navbar() {
     return (
         <div className='navbar'>
+            <div className='nav-account-div'>
+                <h3 className='nav-account-title'>My Account</h3>
+                <img className='nav-profile-placeholder' src={ProfilePlaceholder} alt='logo' height='110' width='110' />
+                <h3 className='nav-account-info'>Name</h3>
+                <h3 className='nav-account-info'>Height</h3>
+                <h3 className='nav-account-info'>Weight</h3>
+            </div>
+            <hr className='nav-line'/>
             <div className='nav-button-div'>
 
                 <NavLink to='/home'>
