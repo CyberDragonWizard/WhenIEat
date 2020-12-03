aca8d73889bc21504c209126da8689ed7438eb31
Brian <briand.nester@gmail.com>
2020-12-02 19:47:50 -0500

    push

--- client/when-i-eat/src/App.js
+++ client/when-i-eat/src/App.js
@@ -1,21 +1,56 @@
-import { Switch, Route} from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Switch, Route, useHistory} from 'react-router-dom';
 import './App.css';
 import Account from './screens/Account';
 import Home from './screens/Home';
 import Login from './screens/Login';
-import Stats from './screens/Stats'
+import Stats from './screens/Stats';
+import { loginUser, registerUser, removeToken, verifyUser } from './services/auth'
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+  const history = useHistory();
+
+  useEffect(() => {
+    const handleVerify = async () => {
+      const userData = await verifyUser();
+      setCurrentUser(userData);
+      if (!userData) {
+        history.push('/')
+      }
+    }
+    handleVerify();
+  }, [])
+
+  const handleLogin = async (loginData) => {
+    const userData = await loginUser(loginData);
+    setCurrentUser(userData);
+    history.push('/');
+  }
+
+  const handleRegister = async (registerData) => {
+    const userData = await registerUser(registerData);
+    setCurrentUser(userData);
+    history.push('/');
+  }
+
+  const handleLogout = () => {
+    setCurrentUser(null);
+    localStorage.removeItem('authToken');
+    removeToken();
+    history.push('/');
+  }
+
   return (
     <Switch>
       <div className="App">
 
         <Route exact path='/'>
-          <Login />
+          <Login handleLogin={handleLogin} handleRegister={handleRegister}/>
         </Route>
 
         <Route exact path='/account'>
-          <Account />
+          <Account currentUser={currentUser} handleLogout={handleLogout}/>
         </Route>
 
         <Route exact path='/home'>
