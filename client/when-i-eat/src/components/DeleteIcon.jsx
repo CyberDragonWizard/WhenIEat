c6886d22b1b6dd9b44bd14f780e5edec99750d34
Brian <briand.nester@gmail.com>
2020-12-05 11:25:41 -0500

    push

--- client/when-i-eat/src/components/DeleteIcon.jsx
+++ client/when-i-eat/src/components/DeleteIcon.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
@@ -20,14 +20,22 @@ export default function DeleteButton(props) {
 
   return (
     <div>
-        <IconButton aria-label="delete" className={classes.margin}>
+      <IconButton aria-label="delete" className={classes.margin}>
+      {
+        props.meals.map(meal => (
+          <React.Fragment key={meal.id}>
           {
-            meal.user_id === props.currentUser
+            meal.user_id === props.currentUser?.id &&
+            <>
           <DeleteIcon 
           onClick={() => props.handleDelete(meal.id)}
           />
+          </>
         }
-        </IconButton>
+        </React.Fragment>
+        ))
+      }
+      </IconButton>
     </div>
   );
 }
